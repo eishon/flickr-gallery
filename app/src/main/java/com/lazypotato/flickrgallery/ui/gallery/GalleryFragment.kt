1989e3434b1d103cdb9402449f9efebcd8fe7788
Ahmed Fahim Foysal Eishon <eishon.dev@gmail.com>
2021-09-12 15:04:59 +0900

    Implemented Search menu

--- app/src/main/java/com/lazypotato/flickrgallery/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/lazypotato/flickrgallery/ui/gallery/GalleryFragment.kt
@@ -1,7 +1,37 @@
 package com.lazypotato.flickrgallery.ui.gallery
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import com.lazypotato.flickrgallery.R
 
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.search, menu)
+
+        val searchItem = menu.findItem(R.id.menu_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+        })
+    }
 }
